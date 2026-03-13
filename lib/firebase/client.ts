a421e39d1c564df9ae22cacc7b25b05ff8b069dc
322bono <322bono@gmail.com>
2026-03-14 03:09:45 +0900

    Inline Firebase web config

--- lib/firebase/client.ts
+++ lib/firebase/client.ts
@@ -4,16 +4,13 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, type Auth } from "firebase/auth";
 import { getFirestore, type Firestore } from "firebase/firestore";
 
-function readPublicEnv(name: string) {
-  return process.env[name] ?? "";
-}
-
 const firebaseConfig = {
-  apiKey: readPublicEnv("NEXT_PUBLIC_FIREBASE_API_KEY"),
-  authDomain: readPublicEnv("NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN"),
-  projectId: readPublicEnv("NEXT_PUBLIC_FIREBASE_PROJECT_ID"),
-  messagingSenderId: readPublicEnv("NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID"),
-  appId: readPublicEnv("NEXT_PUBLIC_FIREBASE_APP_ID")
+  apiKey: "AIzaSyBO7ca34vLXFfnx7kNwEfNpSBkq3d3D3Ik",
+  authDomain: "justdance2-82ec4.firebaseapp.com",
+  projectId: "justdance2-82ec4",
+  storageBucket: "justdance2-82ec4.firebasestorage.app",
+  messagingSenderId: "411635221974",
+  appId: "1:411635221974:web:26c32e0fbbed6eb05f9077"
 };
 
 const firebaseConfigured = Object.values(firebaseConfig).every(Boolean);
